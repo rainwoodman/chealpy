5f6a87fbb742bfe811565ea401e351f681d7cffd
Yu Feng <rainwoodman@gmail.com>
2012-07-11 21:03:59 -0400

    packaging

--- setup.py
+++ setup.py
@@ -2,14 +2,20 @@ from numpy.distutils.core import setup, Extension
 from numpy import get_include
 
 setup(name="chealpy", version="1.0",
+      author="Yu Feng",
+      author_email="yfeng1@andrew.cmu.edu",
+      description="Python Binding of chealpix",
+      license="GPLv2+",
+#      download_url="https://github.com/rainwoodman/chealpy/zipball/master",
+      url="http://github.com/rainwoodman/chealpy",
       package_dir = {'chealpy': 'chealpy'},
       packages = [ 'chealpy' ],
       ext_modules = [
         Extension("chealpy._ccode", 
              ["chealpy/_ccode.c",],
-#             extra_compile_args=['-g -O3'], 
              extra_compile_args=['-O3'],
              libraries=[('chealpix', {'sources':['chealpix.c']})],
+             depends=['chealpix.h'],
              include_dirs=[get_include(), '.'],
         ),
       ])
